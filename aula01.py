59e62b0e8c4c3401f2e9ae4cc754aaa8dc34e40d
duds0901 <duda.mobal@gmail.com>
2024-03-27 10:39:05 -0300

    subir aula01

---
+++ aula01.py
@@ -0,0 +1,10 @@
+import os
+
+#print("Olá mundo cruel")
+print("O 3DDS é a turma número 1 o Sagrada")
+turma= "3DS"
+pos=1
+print("o", turma, "é a turma número", pos, "do Sagrada")
+print(type(turma))
+print(type(pos))
+os.system("clear")
